0ba153b869fa8ea586a783234f6c00ae54b3caad
Meredith Brindley <meredith-brindley@thinkof-us.org>
2022-02-28 19:22:25 -0800

    added sql

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,12 @@
 from flask import Flask
+import sqlite3
 
+#functions
+def startdb():
+    con=sqlite3.connect('test.db')
+
+
+#flask routing
 app= Flask(__name__)
 
 @app.route("/")
@@ -7,4 +14,5 @@ def conn():
     return "Connected Properly"
 
 if __name__ == "__main__":
+    startdb()
     app.run()
