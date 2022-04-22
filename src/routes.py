68c25a57e44840fcbba7971ca722c93bada48a7e
Meredith Brindley <meredith-brindley@thinkof-us.org>
2022-04-21 23:22:14 -0700

    functionality of index added

--- src/routes.py
+++ src/routes.py
@@ -1,20 +1,36 @@
 from email.mime import image
 from turtle import title
 from flask import Flask, render_template
-import sqlite3
+import mysql.connector
 import json
 
 #functions
-def startdb():
-    con=sqlite3.connect('test.db')
+# def startdb():
+#     con=sqlite3.connect('test.db')
+
+def getWaterGDP():
+    cursor.execute('select * from water_gdp')
+    result= cursor.fetchall()
+
+    water_gdp_array= list(map(lambda x: x , result))
+    return water_gdp_array
 
 
 def getCountries():
-    con = sqlite3.connect('country.db')
-    cur = con.cursor()
-    all_countries = cur.execute('select country from countries').fetchall()
-    country_array= list(map(lambda x: x[0] , all_countries))
+    cursor.execute('select * from country')
+    result= cursor.fetchall()
+
+    country_array= list(map(lambda x: (x[1],x[0]) , result))
     return country_array
+
+def getCountryDataSummary(country_id):
+    cursor.execute('select * from data_summary where country_id = '+str(country_id))
+    result=cursor.fetchall()
+
+    countryDataSummary = list(map(lambda x: x , result))
+
+    return countryDataSummary
+
 
 #flask routing
 app= Flask(__name__, static_url_path="/static")
@@ -22,7 +38,8 @@ app= Flask(__name__, static_url_path="/static")
 @app.route("/")
 def conn():
     countries=json.dumps(getCountries())
-    return render_template('index.html', the_title='2seas', countriesList=countries)
+    gdp=json.dumps(getWaterGDP())
+    return render_template('index.html', the_title='2seas', countriesList=countries, waterGdp=gdp)
 
 @app.route("/<country>")
 def path(country):
@@ -38,5 +55,9 @@ def errors():
 
 
 if __name__ == "__main__":
-    startdb()
+    # startdb()
+    cnx = mysql.connector.connect(user='2seas', password='Waterwater2!',
+                                  host='ec2-54-197-15-207.compute-1.amazonaws.com',
+                                  database='twoseas')
+    cursor= cnx.cursor()
     app.run()
