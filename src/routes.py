c2829e00ba7b2f7f22b276475d77791035702c96
Meredith Brindley <meredith-brindley@thinkof-us.org>
2022-04-23 08:26:00 -0700

    most of page 2 functionality

--- src/routes.py
+++ src/routes.py
@@ -15,6 +15,12 @@ def getWaterGDP():
     water_gdp_array= list(map(lambda x: x , result))
     return water_gdp_array
 
+def getregionWaterGDP():
+    cursor.execute('select * from world_water_gdp')
+    result= cursor.fetchall()
+
+    water_gdp_array= list(map(lambda x: x , result))
+    return water_gdp_array
 
 def getCountries():
     cursor.execute('select * from country')
@@ -22,6 +28,13 @@ def getCountries():
 
     country_array= list(map(lambda x: (x[1],x[0]) , result))
     return country_array
+
+def getTargets():
+    cursor.execute('select * from sanitation_targets')
+    result= cursor.fetchall()
+
+    target_array= list(map(lambda x: x , result))
+    return target_array
 
 def getCountryDataSummary(country_id):
     cursor.execute('select * from data_summary where country_id = '+str(country_id))
@@ -39,14 +52,28 @@ app= Flask(__name__, static_url_path="/static")
 def conn():
     countries=json.dumps(getCountries())
     gdp=json.dumps(getWaterGDP())
-    return render_template('index.html', the_title='2seas', countriesList=countries, waterGdp=gdp)
+    regiongdp=json.dumps(getregionWaterGDP())
+    return render_template('index.html', the_title='2seas', countriesList=countries, waterGdp=gdp, regionWaterGdp=regiongdp)
 
 @app.route("/<country>")
 def path(country):
-    print(country)
+    countryList=getCountries()
+    countries=json.dumps(countryList)
+    for place in countryList:
+        tempplace=place[0].replace(" ","_")
+        if tempplace == country:
+            code=place[1]
+        else:
+            errors()
+    dataSummaryList=getCountryDataSummary(code)
+    dataSummary=json.dumps(dataSummaryList)
+    targets=json.dumps(getTargets())
     imageLink= "{{ url_for('static',filename='images/SVG/SVG/"+country+".svg') }}"
-    print(imageLink)
-    return render_template("country.html", the_title=country, image_src=imageLink)
+    return render_template("country.html", the_title=country, image_src=imageLink, countriesList=countries, data_sum=dataSummary, target=targets)
+
+@app.route("/whatcanido/<place>")
+def whatcanido(place):
+    return(place)
 
 @app.route("/404")
 def errors():
